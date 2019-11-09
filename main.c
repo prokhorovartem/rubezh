57ee82afe073a7bcea2ff34edb639dcf56dc3af5
prokhorovartem <art.98.pro@gmail.com>
2019-11-09 08:55:35 +0300

    added .gitignore

--- main.c
+++ main.c
@@ -1,15 +1,5 @@
 #include <stdio.h>
 
-//typedef struct RoughNode {
-//    RoughNode *root;
-//} RoughList;
-//
-//typedef struct {
-//    int key;
-//    int value;
-//    struct RoughNode *next;
-//} RoughNode;
-
 int main() {
     printf("HELLO");
 }
