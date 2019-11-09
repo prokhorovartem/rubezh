43cba0e2a7b0857e6611594c79f0af031ca8955a
daniilkuzcutkomov <kyz9rus@yandex.ru>
2019-11-09 08:48:33 +0300

    project structure

---
+++ cmake-build-debug/CMakeFiles/Rubezh.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Rubezh.dir/main.c.o"
+  "Rubezh.pdb"
+  "Rubezh"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Rubezh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//typedef struct RoughNode {
+//    RoughNode *root;
+//} RoughList;
+//
+//typedef struct {
+//    int key;
+//    int value;
+//    struct RoughNode *next;
+//} RoughNode;
+
+int main() {
+    printf("HELLO");
+}
