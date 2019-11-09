aeec76e46d8690f2cf2f896388e7533859afc348
prokhorovartem <art.98.pro@gmail.com>
2019-11-09 11:33:54 +0300

    azaz

--- task1/main.c
+++ task1/main.c
@@ -1,63 +1,17 @@
-#include <cassert>
 #include "task1.h"
 
-void main() {
-    RoughList *list;
-    list = init();
-    insert(list, 1, 2);
-
-    {
-        FindResult result = find(list, 1);
-        assert(result.exists);
-        assert(result.value == 2);
-    }
-
-    {
-        FindResult result = find(list, 0);
-        assert(!result.exists);
-    }
-
-    insert(list, 0, 1);
-
-    {
-        FindResult result = find(list, 0);
-        assert(result.exists);
-        assert(result.value == 1);
-    }
-
-    insert(list, 2, 3);
-    {
-        FindResult result = find(list, 2);
-        assert(result.exists);
-        assert(result.value == 3);
-    }
-    assert(myRemove(list, 1));
-
-    {
-        FindResult result = find(list, 1);
-        assert(!result.exists);
-    }
-
-    assert(myRemove(list, 0));
-    assert(myRemove(list, 2));
-
-    {
-        FindResult result = find(list, 0);
-        assert(!result.exists);
-    }
-
-    {
-        FindResult result = find(list, 1);
-        assert(!result.exists);
-    }
-
-    {
-        FindResult result = find(list, 2);
-        assert(!result.exists);
-    }
-}
-
 int main() {
-    simple_test();
-    return 0;
+    pthread_mutex_t mutex;
+    pthread_mutex_init(&mutex, NULL);
+    RoughNode node1, node2;
+    node1.key = 1;
+    node1.value = 1;
+    node2.key = 0;
+    node2.value = 2;
+    node2.next = &node1;
+    RoughList list;
+    list.head = &node2;
+    list.tail = &node1;
+    list.mtx = mutex;
+    FindResult find1 = find(&list, 1);
 }
