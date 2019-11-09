1382abd78a0eedca58711b0398b43f3cdd15cc55
Nikita <nikita0levchenko@gmail.com>
2019-11-09 09:12:00 +0300

    Merge branch 'master' of C:\Users\Nikita\CLionProjects\rubezh with conflicts.

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+//typedef struct RoughNode {
+//    RoughNode *root;
+//} RoughList;
+//
+//typedef struct {
+//    int key;
+//    int value;
+//    struct RoughNode *next;
+//} RoughNode;
+
+
+
 int main() {
-    printf("HELLO");
 }
