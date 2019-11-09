f7c84ddb7a4e9c04f280b8857b29bc7b3c12f5ad
Nikita <nikita0levchenko@gmail.com>
2019-11-09 10:59:52 +0300

    task2 delete function

--- task2/task2.c
+++ task2/task2.c
@@ -1,5 +1,5 @@
 #include <pthread.h>
-
+#include <stdlib.h>
 typedef struct ThinNode {
     int key;
     int value;
@@ -22,3 +22,41 @@ char insert(ThinList *list, int key, int value);
 
 char remove(ThinList *list, int key);
 
+char remove(ThinList *list, int key) {
+    ThinNode *prev;
+    ThinNode *curr;
+    char ret = 1;
+
+    pthread_mutex_lock(list->root->mutex);
+
+    prev = list->root;
+    curr = prev->next;
+
+    pthread_mutex_lock(curr->mutex);
+
+    if (curr->next == NULL) {
+        pthread_mutex_unlock(list->root->mutex);
+        pthread_mutex_unlock(curr->mutex);
+        return ret;
+    } else {
+        while (curr->next != NULL && curr->key < key) {
+            pthread_mutex_unlock(prev->mutex);
+            prev = curr;
+            curr = curr->next;
+            pthread_mutex_lock(curr->mutex);
+        }
+
+        if (curr->next != NULL && key == curr->key) {
+            prev->next = curr->next;
+            pthread_mutex_unlock(curr->mutex);
+            free(curr);
+            pthread_mutex_unlock(prev->mutex);
+        } else{
+            pthread_mutex_unlock(prev->mutex);
+            pthread_mutex_unlock(curr->mutex);
+            ret = -1;
+        }
+    }
+
+    return ret;
+}
