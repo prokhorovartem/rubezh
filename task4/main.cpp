a25ae9b3ffb0f5f920a9e9add8883863e8f99b88
daniilkuzcutkomov <kyz9rus@yandex.ru>
2019-12-07 08:56:50 +0300

    refactoring 4,5,6 tasks

--- task4/main.cpp
+++ task4/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-#include <random>
-#include "../include/ThreadPool.h"
+#include "ThreadPool.h"
 
 using namespace std;
 
@@ -9,29 +8,23 @@ using namespace std;
 int a[MAX];
 int part = 0;
 
-// merge function for merging two parts
 void merge(int low, int mid, int high)
 {
     int* left = new int[mid - low + 1];
     int* right = new int[high - mid];
 
-    // n1 is size of left part and n2 is size
-    // of right part
     int n1 = mid - low + 1,
     n2 = high - mid, i, j;
 
-    // storing values in left part
     for (i = 0; i < n1; i++)
         left[i] = a[i + low];
 
-    // storing values in right part
     for (i = 0; i < n2; i++)
         right[i] = a[i + mid + 1];
 
     int k = low;
     i = j = 0;
 
-    // merge left and right in ascending order
     while (i < n1 && j < n2) {
         if (left[i] <= right[j])
             a[k++] = left[i++];
@@ -39,45 +32,35 @@ void merge(int low, int mid, int high)
             a[k++] = right[j++];
     }
 
-    // insert remaining values from left
     while (i < n1) {
         a[k++] = left[i++];
     }
 
-    // insert remaining values from right
     while (j < n2) {
         a[k++] = right[j++];
     }
 }
 
-// merge sort function
 void merge_sort(int low, int high)
 {
-    // calculating mid point of array
     int mid = low + (high - low) / 2;
     if (low < high) {
 
-        // calling first half
         merge_sort(low, mid);
 
-        // calling second half
         merge_sort(mid + 1, high);
 
-        // merging the two halves
         merge(low, mid, high);
     }
 }
 
-// thread function for multi-threading
 void merge_sort_threads()
 {
     int thread_part = part++;
 
-    // calculating low and high
     int low = thread_part * (MAX / THREADS_COUNT);
     int high = (thread_part + 1) * (MAX / THREADS_COUNT) - 1;
 
-    // evaluating mid point
     int mid = low + (high - low) / 2;
     if (low < high) {
         merge_sort(low, mid);
@@ -103,7 +86,6 @@ int main() {
 
     pool.shutdown();
 
-    // merging the final 4 parts
     merge(0, (MAX / 2 - 1) / 2, MAX / 2 - 1);
     merge(MAX / 2, MAX/2 + (MAX-1-MAX/2)/2, MAX - 1);
     merge(0, (MAX - 1)/2, MAX - 1);
